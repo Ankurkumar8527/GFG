e9570447ac38fd2148f5fb16345fc2120eda4c31
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-07-30 14:17:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Friends Pairing Problem/friends-pairing-problem.java
+++ Difficulty: Medium/Friends Pairing Problem/friends-pairing-problem.java
@@ -1,15 +1,7 @@
 class Solution {
-    
-    public int pair(int n, int[] dp ){
-        if(n<=2) return n;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=(pair(n-1,dp)+(n-1)*pair(n-2,dp));
-    }
-    
     public int countFriendsPairings(int n) {
         // code here
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return pair(n,dp);
+        if(n<=2) return n;
+        return countFriendsPairings(n-1)+(n-1)*countFriendsPairings(n-2);
     }
 }
