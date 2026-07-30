79bdc0d956b034c7ec2195044f71fbc65066a21a
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-07-30 14:57:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Derangements/count-derangements.java
+++ Difficulty: Medium/Count Derangements/count-derangements.java
@@ -1,15 +1,7 @@
 class Solution {
     public int derangeCount(int n) {
         // code here
-        // Tabulation method
-        if(n==1) return 0;
         if(n<=3) return n-1;
-        int[] dp = new int[n+1];
-        dp[2]=1;
-        dp[3]=2;
-        for(int i=4;i<=n;i++){
-            dp[i]=(i-1)*(dp[i-1]+dp[i-2]);
-        }
-        return dp[n];
+        return (n-1)*(derangeCount(n-1)+derangeCount(n-2));
     }
 };
