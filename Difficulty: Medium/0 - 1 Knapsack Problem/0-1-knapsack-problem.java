55fa4950216c47de389e48804ffc8d81f93b3761
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-08-13 16:44:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,19 +1,19 @@
 class Solution {
-    public int MaxProfit(int i,int C, int val[], int wt[],int[][] dp){
-        if(i==val.length) return 0;
-        if(dp[i][C]!=-1) return dp[i][C];
-        int skip = MaxProfit(i+1,C,val,wt,dp);
-        if(wt[i]>C) return dp[i][C] = skip;
-        int take = val[i]+MaxProfit(i+1,C-wt[i],val,wt,dp);
-        return dp[i][C] = Math.max(skip,take);
-    }
-    public int knapsack(int C, int val[], int wt[]) {
+    public int knapsack(int W, int val[], int wt[]) {
         // code here
         int n = val.length;
-        int[][] dp = new int[n][C+1];
+        int[][] dp = new int[n][W+1];
         for(int i=0;i<n;i++){
-            for(int j=0;j<C+1;j++) dp[i][j]=-1;
+            for(int j=0;j<=W;j++){
+                int skip = (i>0) ? dp[i-1][j]:0;
+                if(wt[i]>j) dp[i][j]=skip;
+                else{
+                    int pick = val[i];
+                    pick += ((i>0) ? dp[i-1][j-wt[i]] : 0);
+                    dp[i][j]= Math.max(pick,skip);
+                }
+            }
         }
-        return MaxProfit(0,C,val,wt,dp);
+        return dp[n-1][W];
     }
 }
